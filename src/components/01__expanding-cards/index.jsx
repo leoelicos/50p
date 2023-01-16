b1bee6858ecb8c61588e506774d0c332348a7bef
Leo Wong <leoelicos@gmail.com>
2023-01-16 23:28:47 +1100

    Green - useState

--- src/components/01__expanding-cards/index.jsx
+++ src/components/01__expanding-cards/index.jsx
@@ -1,3 +1,39 @@
+import { useState } from 'react'
+import './style/style.css'
+
 export default function ExpandingCards() {
-  return <div>ExpandingCards</div>
+  const cards = new Array(5).fill('https://picsum.photos/1000/1000')
+  const [active, setActive] = useState(null)
+  const handleClick = (i) => {
+    console.log('setting ', i, 'active')
+    setActive(i)
+  }
+  return (
+    <App>
+      <Body>
+        <PanelContainer>
+          {cards.map((card, i) => (
+            <Panel
+              key={i}
+              card={card}
+              i={i}
+              onClick={() => handleClick(i)}
+              active={i === active}
+            />
+          ))}
+        </PanelContainer>
+      </Body>
+    </App>
+  )
 }
+const App = ({ children }) => <div className='app_01'>{children}</div>
+const Body = ({ children }) => <div className='body'>{children}</div>
+const PanelContainer = ({ children }) => <div className='container'>{children}</div>
+const Panel = ({ card, i, onClick, active }) => (
+  <div
+    className={`panel ${active ? 'active' : ''}`}
+    style={{ backgroundImage: `url(${card})` }}
+    onClick={onClick}>
+    <h3>{`Card ${i + 1}`}</h3>
+  </div>
+)
