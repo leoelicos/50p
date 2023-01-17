1738fd26f97a7d49b6dbbe21b52b7eda3e1ac140
Leo Wong <leoelicos@gmail.com>
2023-01-17 23:00:36 +1100

    Starter JS code

--- src/components/03__rotating-navigation-animation/index.jsx
+++ src/components/03__rotating-navigation-animation/index.jsx
@@ -43,3 +43,10 @@ export default function RotatingNavigationAnimation() {
     </div>
   )
 }
+
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
+
+open.addEventListener('click', () => container.classList.add('show-nav'))
+close.addEventListener('click', () => container.classList.remove('show-nav'))
