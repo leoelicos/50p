b824e320529fd707c991ea2d9d73bfee502aff2f
Leo Wong <leoelicos@gmail.com>
2023-01-18 01:16:18 +1100

    Starter HTML and Commented Starter JS

--- src/components/04__hidden-search-widget/index.jsx
+++ src/components/04__hidden-search-widget/index.jsx
@@ -1,3 +1,31 @@
 export default function HiddenSearchWidget() {
-  return <div>HiddenSearchWidget</div>
+  return (
+    <div className='app-04'>
+      <div className='body'>
+        <div className='search'>
+          <input
+            type='text'
+            className='input'
+            placeholder='Search...'
+          />
+          <button className='btn'>
+            <i className='fas fa-search'></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
+/* 
+
+const search = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+
+btn.addEventListener('click', () => {
+	search.classList.toggle('active');
+	input.focus();
+});
+
+
+*/
