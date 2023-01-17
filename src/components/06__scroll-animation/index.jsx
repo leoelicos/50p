93e621be8ead928301a4b678d8797077d137be8e
Leo Wong <leoelicos@gmail.com>
2023-01-18 08:14:15 +1100

    Starter HTML with commented Starter JS

--- src/components/06__scroll-animation/index.jsx
+++ src/components/06__scroll-animation/index.jsx
@@ -1,3 +1,50 @@
 export default function ScrollAnimation() {
-  return <div>ScrollAnimation</div>
+  return (
+    <div className='app-06'>
+      <div className='body'>
+        <h1>Scroll to see the animation</h1>
+        <div className='box'>
+          <h2>Content</h2>
+        </div>
+        <div className='box'>
+          <h2>Content</h2>
+        </div>
+        <div className='box'>
+          <h2>Content</h2>
+        </div>
+        <div className='box'>
+          <h2>Content</h2>
+        </div>
+        <div className='box'>
+          <h2>Content</h2>
+        </div>
+        <div className='box'>
+          <h2>Content</h2>
+        </div>
+      </div>
+    </div>
+  )
 }
+
+/* 
+
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+	const triggerBottom = (window.innerHeight / 5) * 4;
+	boxes.forEach((box) => {
+		const boxTop = box.getBoundingClientRect().top;
+		if (boxTop < triggerBottom) {
+			box.classList.add('show');
+		} else {
+			box.classList.remove('show');
+		}
+	});
+}
+
+
+*/
