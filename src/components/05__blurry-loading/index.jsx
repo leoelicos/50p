ea6b8d539416adc1b9eaa0c21abb7307c4c8c469
Leo Wong <leoelicos@gmail.com>
2023-01-18 08:01:53 +1100

    Starter HTML with commented Starter JS

--- src/components/05__blurry-loading/index.jsx
+++ src/components/05__blurry-loading/index.jsx
@@ -1,3 +1,41 @@
 export default function BlurryLoading() {
-  return <div>BlurryLoading</div>
+  return (
+    <App>
+      <Body>
+        <section className='bg'></section>
+        <div className='loading-text'>0%</div>
+      </Body>
+    </App>
+  )
 }
+
+const App = ({ children }) => <div className='app-05'>{children}</div>
+const Body = ({ children }) => <div className='body'>{children}</div>
+
+/* 
+
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+
+let int = setInterval(blurring, 30);
+
+function blurring() {
+	load++;
+
+	if (load > 99) {
+		clearInterval(int);
+	}
+
+	loadText.innerText = `${load}%`;
+	loadText.style.opacity = scale(load, 0, 100, 1, 0);
+	bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+	return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
+
+*/
