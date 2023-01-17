b3e9e9a7385fa48105d8ee54adb0e670be19541a
Leo Wong <leoelicos@gmail.com>
2023-01-18 08:11:27 +1100

    Implement with useEffect, useState

--- src/components/05__blurry-loading/index.jsx
+++ src/components/05__blurry-loading/index.jsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from 'react'
 import './style/style.css'
 
 export default function BlurryLoading() {
+  const [loadtext, setLoadtext] = useState(null)
+  const [opacity, setOpacity] = useState(null)
+  const [bgFilter, setBgFilter] = useState(null)
+
+  useEffect(() => {
+    let load = 0
+    let int = setInterval(blurring, 30)
+    function blurring() {
+      load++
+      if (load > 99) clearInterval(int)
+      setLoadtext(`${load}%`)
+      setOpacity(scale(load, 0, 100, 1, 0))
+      setBgFilter(`blur(${scale(load, 0, 100, 30, 0)}px)`)
+    }
+  }, [])
+
   return (
     <App>
       <Body>
-        <section className='bg'></section>
-        <div className='loading-text'>0%</div>
+        <section
+          className='bg'
+          style={{ filter: bgFilter }}></section>
+        <div
+          className='loading-text'
+          style={{ opacity }}>
+          {loadtext}
+        </div>
       </Body>
     </App>
   )
@@ -14,30 +37,6 @@ export default function BlurryLoading() {
 const App = ({ children }) => <div className='app-05'>{children}</div>
 const Body = ({ children }) => <div className='body'>{children}</div>
 
-/* 
-
-const loadText = document.querySelector('.loading-text');
-const bg = document.querySelector('.bg');
-
-let load = 0;
-
-let int = setInterval(blurring, 30);
-
-function blurring() {
-	load++;
-
-	if (load > 99) {
-		clearInterval(int);
-	}
-
-	loadText.innerText = `${load}%`;
-	loadText.style.opacity = scale(load, 0, 100, 1, 0);
-	bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
-}
-
 const scale = (num, in_min, in_max, out_min, out_max) => {
-	return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-};
-
-
-*/
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
