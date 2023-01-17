83a233d242d68e9963b73a05ebf5fbc32e9aa8de
Leo Wong <leoelicos@gmail.com>
2023-01-18 01:36:02 +1100

    Implement with useRef and useState

--- src/components/04__hidden-search-widget/index.jsx
+++ src/components/04__hidden-search-widget/index.jsx
@@ -1,33 +1,44 @@
 import './style/style.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { useRef, useState } from 'react'
 
 export default function HiddenSearchWidget() {
+  const [active, setActive] = useState(false)
+  const inputEl = useRef(null)
+  const buttonEl = useRef(null)
+  const handleClickSearch = () => {
+    setActive((prev) => !prev)
+    if (!active) inputEl.current.focus()
+    else buttonEl.current.focus()
+  }
+  const [value, setValue] = useState('')
+  const handleValueChange = (e) => {
+    setValue(e.target.value)
+  }
   return (
-    <div className='app-04'>
-      <div className='body'>
-        <div className='search'>
+    <App>
+      <Body>
+        <Search active={active}>
           <input
             type='text'
             className='input'
             placeholder='Search...'
+            value={value}
+            onChange={handleValueChange}
+            ref={inputEl}
           />
-          <button className='btn'>
-            <i className='fas fa-search'></i>
+          <button
+            className='btn'
+            onClick={handleClickSearch}
+            ref={buttonEl}>
+            <FontAwesomeIcon icon={faSearch} />
           </button>
-        </div>
-      </div>
-    </div>
+        </Search>
+      </Body>
+    </App>
   )
 }
-/* 
-
-const search = document.querySelector('.search');
-const btn = document.querySelector('.btn');
-const input = document.querySelector('.input');
-
-btn.addEventListener('click', () => {
-	search.classList.toggle('active');
-	input.focus();
-});
-
-
-*/
+const App = ({ children }) => <div className='app-04'>{children}</div>
+const Body = ({ children }) => <div className='body'>{children}</div>
+const Search = ({ children, active }) => <div className={`search ${active ? 'active' : ''}`}>{children}</div>
