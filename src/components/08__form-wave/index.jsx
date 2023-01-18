435f10f34f106276a605bf502625938f24ab7354
Leo Wong <leoelicos@gmail.com>
2023-01-18 20:38:37 +1100

    Starter HTML and starter JS commented

--- src/components/08__form-wave/index.jsx
+++ src/components/08__form-wave/index.jsx
@@ -1,3 +1,39 @@
 export default function FormWave() {
   return <div>FormWave</div>
 }
+
+/* 
+
+		<div class="container">
+			<h1>Please Login</h1>
+			<form>
+				<div class="form-control">
+					<input type="text" required />
+					<label>Email</label>
+				</div>
+				<div class="form-control">
+					<input type="password" required />
+					<label>Password</label>
+				</div>
+				<button class="btn">Login</button>
+				<p class="text">Don't have an account? <a href="#">Register</a></p>
+			</form>
+		</div>
+    
+    */
+
+/* 
+    
+    
+    const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach((label) => {
+	label.innerHTML = label.innerText
+		.split('')
+		.map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+		.join('');
+});
+
+
+
+*/
