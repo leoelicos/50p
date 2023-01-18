dd919b2e8a2e2f5216b6b9bc5f0cce46beea57c9
Leo Wong <leoelicos@gmail.com>
2023-01-18 19:58:20 +1100

    Starter HTML, with commented Starter JS

--- src/components/07__split-landing-page/index.jsx
+++ src/components/07__split-landing-page/index.jsx
@@ -1,3 +1,33 @@
 export default function SplitLandingPage() {
   return <div>SplitLandingPage</div>
 }
+
+/* 
+	<body>
+		<div class="container">
+			<div class="split left">
+				<h1>Playstation 5</h1>
+				<a href="#" class="btn">Buy Now</a>
+			</div>
+			<div class="split right">
+				<h1>XBox Series X</h1>
+				<a href="#" class="btn">Buy Now</a>
+			</div>
+		</div>
+
+		<script src="./script.js"></script>
+	</body>
+   */
+
+/* 
+
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const container = document.querySelector('.container');
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'));
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'));
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'));
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
+
+*/
