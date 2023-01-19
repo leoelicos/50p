6b7ba7d0719d09fc43867485671e87d4a1845134
Leo Wong <leoelicos@gmail.com>
2023-01-19 15:16:24 +1100

    Implement with useEffect, useState

--- src/components/11__event-key-codes/index.jsx
+++ src/components/11__event-key-codes/index.jsx
@@ -1,33 +1,47 @@
+import { useEffect, useState } from 'react'
+import './style/style.css'
+
 export default function EventKeyCodes() {
+  const [k, setK] = useState(null)
+
+  const handleKeyDown = (e) => {
+    const key = e.key === ' ' ? 'Space' : e.key
+    const code = e.code
+    const keyCode = e.keyCode
+    setK({ key, code, keyCode })
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown)
+  }, [])
+
   return (
     <div className='app-11'>
       <div className='body'>
-        <div id='insert'>
-          <div className='key'>Press any key to get the keyCode</div>
-        </div>
+        <Insert k={k} />
       </div>
     </div>
   )
 }
-
-/* 
-
-const insert = document.getElementById('insert');
-
-window.addEventListener('keydown', (event) => {
-	insert.innerHTML = `			<div class="key">
-    ${event.key === ' ' ? 'Space' : event.key}
-    <small>event.key</small>
-</div>
-
-<div class="key">
-    ${event.keyCode}
-    <small>event.keyCode</small>
-</div>
-<div class="key">
-    ${event.code}
-    <small>event.code</small>
-</div>`;
-});
-
-*/
+function Insert({ k }) {
+  return k === null ? (
+    <div id='insert'>
+      <div className='key'>Press any key to get the keyCode</div>
+    </div>
+  ) : (
+    <div id='insert'>
+      <div className='key'>
+        {k.key}
+        <small>event.key</small>
+      </div>
+      <div className='key'>
+        {k.keyCode}
+        <small>event.keyCode</small>
+      </div>
+      <div className='key'>
+        {k.code}
+        <small>event.code</small>
+      </div>
+    </div>
+  )
+}
