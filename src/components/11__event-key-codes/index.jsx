22c8d392ef597982f62ce129d0f75ad79e19c5ad
Leo Wong <leoelicos@gmail.com>
2023-01-19 14:30:11 +1100

    Starter code

--- src/components/11__event-key-codes/index.jsx
+++ src/components/11__event-key-codes/index.jsx
@@ -1,3 +1,33 @@
 export default function EventKeyCodes() {
-  return <div>EventKeyCodes</div>
+  return (
+    <div className='app-11'>
+      <div className='body'>
+        <div id='insert'>
+          <div className='key'>Press any key to get the keyCode</div>
+        </div>
+      </div>
+    </div>
+  )
 }
+
+/* 
+
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', (event) => {
+	insert.innerHTML = `			<div class="key">
+    ${event.key === ' ' ? 'Space' : event.key}
+    <small>event.key</small>
+</div>
+
+<div class="key">
+    ${event.keyCode}
+    <small>event.keyCode</small>
+</div>
+<div class="key">
+    ${event.code}
+    <small>event.code</small>
+</div>`;
+});
+
+*/
