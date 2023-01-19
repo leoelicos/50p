5ba0bdfa19ecffdeb3f423b9dd360ae1b123c84b
Leo Wong <leoelicos@gmail.com>
2023-01-19 21:26:23 +1100

    Starter HTML with className JSX

--- src/components/14__animated-navigation/index.jsx
+++ src/components/14__animated-navigation/index.jsx
@@ -1,3 +1,32 @@
 export default function AnimatedNavigation() {
-  return <div>AnimatedNavigation</div>
+  return (
+    <div className='app-14'>
+      <div className='body'>
+        <nav
+          className='active'
+          id='nav'>
+          <ul>
+            <li>
+              <a href=''>Home</a>
+            </li>
+            <li>
+              <a href=''>Works</a>
+            </li>
+            <li>
+              <a href=''>About</a>
+            </li>
+            <li>
+              <a href=''>Contact</a>
+            </li>
+          </ul>
+          <button
+            className='icon'
+            id='toggle'>
+            <div className='line line1'></div>
+            <div className='line line2'></div>
+          </button>
+        </nav>
+      </div>
+    </div>
+  )
 }
