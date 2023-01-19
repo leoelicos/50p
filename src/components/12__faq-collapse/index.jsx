654ea3a1101ac6ea7e2a50f39b6d879c78c8dae0
Leo Wong <leoelicos@gmail.com>
2023-01-19 15:47:42 +1100

    Implement FAQCollapse with useState

--- src/components/12__faq-collapse/index.jsx
+++ src/components/12__faq-collapse/index.jsx
@@ -1,77 +1,56 @@
-export default function FAQCollapse() {
+import { useState } from 'react'
+import './style/style.css'
+function App({ children }) {
+  return <div className='app-12'>{children}</div>
+}
+
+function Body({ children }) {
+  return <div className='body'>{children}</div>
+}
+
+function FAQContainer({ children }) {
+  return <div className='faq-container'>{children}</div>
+}
+
+const data = [
+  { id: 1, title: "Why shouldn't we trust atoms?", text: 'They make up everything.' },
+  { id: 2, title: 'What do you call someone with no body and no nose?', text: 'Nobody knows.' },
+  { id: 3, title: "What's the object-oriented way to become wealthy?", text: 'Inheritance.' },
+  { id: 4, title: 'How many tickles does it take to tickle an octopus?', text: 'Ten-tickles?' },
+  { id: 5, title: 'What is: 1 + 1?', text: 'Depends on who you are asking.' }
+]
+function FAQ({ joke, isFirst }) {
+  const [active, setActive] = useState(isFirst ? true : false)
   return (
-    <div className='app-12'>
-      <div className='body'>
-        <h1>Frequently Asked Questions</h1>
-
-        <div className='faq-container'>
-          <div className='faq active'>
-            <h3 className='faq-title'>Why shouldn't we trust atoms?</h3>
-
-            <p className='faq-text'>They make up everything.</p>
-
-            <button className='faq-toggle'>
-              <i className='fas fa-chevron-down'></i>
-              <i className='fas fa-times'></i>
-            </button>
-          </div>
-
-          <div className='faq'>
-            <h3 className='faq-title'>What do you call someone with no body and no nose?</h3>
-
-            <p className='faq-text'>Nobody knows.</p>
-
-            <button className='faq-toggle'>
-              <i className='fas fa-chevron-down'></i>
-              <i className='fas fa-times'></i>
-            </button>
-          </div>
-
-          <div className='faq'>
-            <h3 className='faq-title'>What's the object-oriented way to become wealthy?</h3>
-
-            <p className='faq-text'>Inheritance.</p>
-
-            <button className='faq-toggle'>
-              <i className='fas fa-chevron-down'></i>
-              <i className='fas fa-times'></i>
-            </button>
-          </div>
-
-          <div className='faq'>
-            <h3 className='faq-title'>How many tickles does it take to tickle an octopus?</h3>
-
-            <p className='faq-text'>Ten-tickles?</p>
-
-            <button className='faq-toggle'>
-              <i className='fas fa-chevron-down'></i>
-              <i className='fas fa-times'></i>
-            </button>
-          </div>
-
-          <div className='faq'>
-            <h3 className='faq-title'>What is: 1 + 1?</h3>
-
-            <p className='faq-text'>Depends on who you are asking.</p>
-
-            <button className='faq-toggle'>
-              <i className='fas fa-chevron-down'></i>
-              <i className='fas fa-times'></i>
-            </button>
-          </div>
-        </div>
-      </div>
+    <div className={`faq ${active ? 'active' : ''}`}>
+      <h3 className='faq-title'>{joke.title}</h3>
+      <p className='faq-text'>{joke.text}</p>
+      <button
+        className='faq-toggle'
+        onClick={() => {
+          setActive((prev) => !prev)
+        }}>
+        <i className='fas fa-chevron-down'></i>
+        <i className='fas fa-times'></i>
+      </button>
     </div>
   )
 }
-
-//  bring in the toggle buttons using querySelectorAll with class faq-toggle, this will create a node list
-// loop through nodelist (forEach)
-// on each toggle, add a click event (addEvent Listener)
-// toggle the active class on the parent node (.parentNode & classList.toggle())
-const nodeList = document.querySelectorAll('.faq-toggle')
-nodeList.forEach((toggle) => {
-  toggle.addEventListener('click', () => {
-    toggle.parentNode.classList.toggle('active')
-  })
-})
+export default function FAQCollapse() {
+  return (
+    <App>
+      <Body>
+        <h1>Frequently Asked Questions</h1>
+        <FAQContainer>
+          {data.map((joke, i) => (
+            <FAQ
+              key={joke.id}
+              joke={joke}
+              isFirst={i === 0}
+            />
+          ))}
+        </FAQContainer>
+      </Body>
+    </App>
+  )
+}
