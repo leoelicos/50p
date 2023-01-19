cec63af27f24b586c81b89f2c425aacc4232191c
Leo Wong <leoelicos@gmail.com>
2023-01-19 13:36:33 +1100

    Starter HTML to JSX

--- src/components/10__dad-jokes/index.jsx
+++ src/components/10__dad-jokes/index.jsx
@@ -1,3 +1,19 @@
 export default function DadJokes() {
-  return <div>DadJokes</div>
+  return (
+    <div className='app-10'>
+      <div className='body'>
+        <div className='container'>
+          <h3>Don't Laugh Challenge</h3>
+          <div
+            id='joke'
+            className='joke'></div>
+          <button
+            id='jokeBtn'
+            className='btn'>
+            Get Another Joke
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
