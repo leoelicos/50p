f01c261db9dff42901fbe3ff833904ecb88506ad
Leo Wong <leoelicos@gmail.com>
2023-01-21 02:00:43 +1100

    Implement with useState

--- src/components/21__drag-n-drop/index.jsx
+++ src/components/21__drag-n-drop/index.jsx
@@ -1,3 +1,59 @@
+import { useState } from 'react'
+import './style/style.css'
+
+const data21 = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }]
 export default function DragNDrop() {
-  return <div>DragNDrop</div>
+  const [active, setActive] = useState(1)
+
+  return (
+    <div className='app-21'>
+      {data21.map(({ id }) => (
+        <Empty
+          key={id}
+          id={id}
+          active={active}
+          setActive={setActive}
+        />
+      ))}
+    </div>
+  )
+}
+
+const Empty = ({ id, active, setActive }) => {
+  const [cName, setCName] = useState('empty')
+  const [pName, setPName] = useState('fill')
+  return (
+    <div
+      key={id}
+      className={cName}
+      onDragOver={(e) => {
+        e.preventDefault()
+      }}
+      onDragEnter={(e) => {
+        e.preventDefault()
+        setCName((prev) => prev + ' hovered')
+      }}
+      onDragLeave={() => {
+        setCName('empty')
+      }}
+      onDrop={() => {
+        setCName('empty')
+        setActive(id)
+      }}>
+      {active === id ? (
+        <div
+          className={pName}
+          draggable='true'
+          onDragStart={() => {
+            setPName('fill  ')
+            // setTimeout(() => {
+            //   setPName('invisible')
+            // }, 0)
+          }}
+          onDragEnd={() => {
+            setPName('fill')
+          }}></div>
+      ) : null}
+    </div>
+  )
 }
