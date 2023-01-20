d4a0eedca67fece5394f77a70d4d4694952c37ae
Leo Wong <leoelicos@gmail.com>
2023-01-21 01:01:11 +1100

    Implement with useSstate

--- src/components/20__button-ripple-effect/index.jsx
+++ src/components/20__button-ripple-effect/index.jsx
@@ -1,3 +1,39 @@
+import { useState } from 'react'
+import './style/style.css'
+
 export default function ButtonRippleEffect() {
-  return <div>ButtonRippleEffect</div>
+  const [xInside, setXInside] = useState(0)
+  const [yInside, setYInside] = useState(0)
+  const [active, setActive] = useState(false)
+  return (
+    <div className='app-20'>
+      <div className='body'>
+        <button
+          className='ripple'
+          onClick={(e) => {
+            const x = e.clientX
+            const y = e.clientY
+
+            const buttonTop = e.target.offsetTop
+            const buttonLeft = e.target.offsetLeft
+
+            setXInside(x - buttonLeft)
+            setYInside(y - buttonTop)
+
+            setActive(true)
+            setTimeout(() => {
+              setActive(false)
+            }, 500)
+          }}>
+          Click Me
+          {active ? (
+            <span
+              className='circle'
+              style={{ top: `${yInside}px`, left: `${xInside}px` }}
+            />
+          ) : null}
+        </button>
+      </div>
+    </div>
+  )
 }
