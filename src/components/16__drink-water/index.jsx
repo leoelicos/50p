36e111bcb1cc25a1ad0c9f192f95c1d730b87383
Leo Wong <leoelicos@gmail.com>
2023-01-20 19:55:07 +1100

    Implement hide and text of remained and percentage

--- src/components/16__drink-water/index.jsx
+++ src/components/16__drink-water/index.jsx
@@ -8,21 +8,44 @@ export default function DrinkWater() {
   const totalCups = 8
   const ratio = fullCups / totalCups
 
+  const remained = {
+    style:
+      fullCups === totalCups
+        ? { visibility: 'hidden', height: '0' } //
+        : { visibility: 'visible' },
+    text:
+      fullCups === totalCups //
+        ? ''
+        : `${2 - (250 * fullCups) / 1000}L`
+  }
+
+  const percentage = {
+    style:
+      fullCups === 0 //
+        ? { visibility: 'hidden', height: '0' }
+        : { visibility: 'visible', height: `${ratio * 330}px` },
+    text:
+      fullCups === 0
+        ? '' //
+        : `${ratio * 100}%`
+  }
   return (
     <div className='app-16'>
       <div className='body'>
         <h1>Drink Water</h1>
         <h3>Goal: 2 Liters</h3>
         <div className='cup'>
-          <div className='remained'>
-            <span id='liters style'>{2 - (250 * fullCups) / 1000}L</span>
+          <div
+            className='remained'
+            style={remained.style}>
+            <span>{remained.text}</span>
             <small>Remained</small>
           </div>
 
           <div
             className='percentage'
-            style={fullCups === 0 ? { visibility: 'hidden', height: '0' } : { visibility: 'visible', height: `${ratio * 330}px` }}>
-            {fullCups === 0 ? '' : `${ratio * 100}%`}
+            style={percentage.style}>
+            {percentage.text}
           </div>
         </div>
         <p className='text'>Select how many glasses of water that you have drunk</p>
