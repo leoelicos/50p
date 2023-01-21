2dd0598f2115f89e29ea2a5db60dda6a29cd3a6d
Leo Wong <leoelicos@gmail.com>
2023-01-22 00:35:58 +1100

    Starter HTML

--- src/components/27__toast-notification/index.jsx
+++ src/components/27__toast-notification/index.jsx
@@ -1,3 +1,17 @@
 export default function ToastNotification() {
-  return <div>ToastNotification</div>
+  return (
+    <div className='app-27'>
+      <div className='body'>
+        <div id='toasts'>
+          <div className='toast'>Hello World</div>
+          <div className='toast'>Toast Number 2</div>
+        </div>
+        <button
+          className='btn'
+          id='button'>
+          Show Notification
+        </button>
+      </div>
+    </div>
+  )
 }
