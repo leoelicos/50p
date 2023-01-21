6107b585706058831b79ff2585e13ee0e40755a3
Leo Wong <leoelicos@gmail.com>
2023-01-21 21:09:03 +1100

    Extract data to external file

---
+++ src/components/24__content-placeholder/data/index.js
@@ -0,0 +1,19 @@
+const data = {
+  header: (
+    <img
+      src='https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWqiOjEyMDd9@auto=format&fit=crop&w=2102&q=80'
+      alt=''
+    />
+  ),
+  title: 'Lorem ipsum dolor sit amet.',
+  excerpt: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, aspernatur. ',
+  profile_img: (
+    <img
+      src='https://randomuser.me/api/portraits/men/45.jpg'
+      alt=''
+    />
+  ),
+  name: 'John Doe',
+  date: 'Oct 08, 2020'
+}
+export default data
--- src/components/24__content-placeholder/index.jsx
+++ src/components/24__content-placeholder/index.jsx
@@ -1,23 +1,6 @@
 import { useEffect, useState } from 'react'
 import './style/style.css'
-const data = {
-  header: (
-    <img
-      src='https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWqiOjEyMDd9@auto=format&fit=crop&w=2102&q=80'
-      alt=''
-    />
-  ),
-  title: 'Lorem ipsum dolor sit amet.',
-  excerpt: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, aspernatur. ',
-  profile_img: (
-    <img
-      src='https://randomuser.me/api/portraits/men/45.jpg'
-      alt=''
-    />
-  ),
-  name: 'John Doe',
-  date: 'Oct 08, 2020'
-}
+import data from './data/index'
 export default function ContentPlaceholder() {
   const [loading, setLoading] = useState(true)
   useEffect(() => {
