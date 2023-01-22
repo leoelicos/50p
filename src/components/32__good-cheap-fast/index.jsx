81858babe5c04957113f76579b4a2c5fb3509eb8
Leo Wong <leoelicos@gmail.com>
2023-01-23 03:02:31 +1100

    Refactored to separate concerns

--- src/components/32__good-cheap-fast/index.jsx
+++ src/components/32__good-cheap-fast/index.jsx
@@ -1,74 +1,68 @@
 import { useState } from 'react'
 import './style/style.css'
 
+const ToggleContainer = ({ text, checked, handleChange }) => (
+  <div className='toggle-container'>
+    <input
+      type='checkbox'
+      id={text}
+      className='toggle'
+      checked={checked}
+      onChange={handleChange}
+    />
+    <label
+      htmlFor={text}
+      className='label'>
+      <div className='ball' />
+    </label>
+    <span>{text}</span>
+  </div>
+)
+
 export default function GoodCheapFast() {
   const [good, setGood] = useState(false)
   const [cheap, setCheap] = useState(false)
   const [fast, setFast] = useState(false)
+
+  const handleChangeGood = (e) => {
+    setGood(() => {
+      if (cheap && fast) setFast(false)
+      return e.target.checked
+    })
+  }
+
+  const handleChangeCheap = (e) => {
+    setCheap(() => {
+      if (good && fast) setGood(false)
+      return e.target.checked
+    })
+  }
+
+  const handleChangeFast = (e) => {
+    setFast(() => {
+      if (good && cheap) setCheap(false)
+      return e.target.checked
+    })
+  }
+
+  const boxes = [
+    { text: 'Good', checked: good, handleChange: handleChangeGood },
+    { text: 'Cheap', checked: cheap, handleChange: handleChangeCheap },
+    { text: 'Fast', checked: fast, handleChange: handleChangeFast }
+  ]
+
   return (
     <div className='app-32'>
       <div className='body'>
         <h2>How do you want your project to be?</h2>
-        <div className='toggle-container'>
-          <input
-            type='checkbox'
-            id='good'
-            className='toggle'
-            checked={good}
-            onChange={(e) => {
-              setGood(() => {
-                if (cheap && fast) setFast(false)
-                return e.target.checked
-              })
-            }}
+        {boxes.map(({ text, checked, handleChange }) => (
+          <ToggleContainer
+            key={text}
+            text={text}
+            checked={checked}
+            handleChange={handleChange}
           />
-          <label
-            htmlFor='good'
-            className='label'>
-            <div className='ball'></div>
-          </label>
-          <span>Good</span>
-        </div>
-        <div className='toggle-container'>
-          <input
-            type='checkbox'
-            id='cheap'
-            className='toggle'
-            checked={cheap}
-            onChange={(e) => {
-              setCheap(() => {
-                if (good && fast) setGood(false)
-                return e.target.checked
-              })
-            }}
-          />
-          <label
-            htmlFor='cheap'
-            className='label'>
-            <div className='ball'></div>
-          </label>
-          <span>Cheap</span>
-        </div>
-        <div className='toggle-container'>
-          <input
-            type='checkbox'
-            id='fast'
-            className='toggle'
-            checked={fast}
-            onChange={(e) => {
-              setFast(() => {
-                if (good && cheap) setCheap(false)
-                return e.target.checked
-              })
-            }}
-          />
-          <label
-            htmlFor='fast'
-            className='label'>
-            <div className='ball'></div>
-          </label>
-          <span>Fast</span>
-        </div>
+        ))}
       </div>
     </div>
   )
