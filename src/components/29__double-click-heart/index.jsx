5ad2f1d54157e9fd447d42a7970a7b63642747d1
Leo Wong <leoelicos@gmail.com>
2023-01-22 23:19:08 +1100

    Starter HTML and JS

--- src/components/29__double-click-heart/index.jsx
+++ src/components/29__double-click-heart/index.jsx
@@ -1,3 +1,58 @@
 export default function DoubleClickHeart() {
-  return <div>DoubleClickHeart</div>
+  return (
+    <div className='app-29'>
+      <div className='body'>
+        <h3>
+          Double click on the image to <i className='fas fa-heart'></i> it
+        </h3>
+        <small>
+          You liked it <span id='times'>0</span> times
+        </small>
+        <div className='loveMe'></div>
+      </div>
+    </div>
+  )
+}
+
+const loveMe = document.querySelector('.loveMe')
+const times = document.querySelector('#times')
+
+let clickTime = 0
+let timesClicked = 0
+
+loveMe.addEventListener('click', (e) => {
+  if (clickTime === 0) {
+    clickTime = new Date().getTime()
+  } else {
+    if (new Date().getTime() - clickTime < 800) {
+      createHeart(e)
+      clickTime = 0
+    } else {
+      clickTime = new Date().getTime()
+    }
+  }
+})
+
+const createHeart = (e) => {
+  const heart = document.createElement('i')
+  heart.classList.add('fas')
+  heart.classList.add('fa-heart')
+
+  const x = e.clientX
+  const y = e.clientY
+
+  const leftOffset = e.target.offsetLeft
+  const topOffset = e.target.offsetTop
+
+  const xInside = x - leftOffset
+  const yInside = y - topOffset
+
+  heart.style.top = `${yInside}px`
+  heart.style.left = `${xInside}px`
+
+  loveMe.appendChild(heart)
+
+  times.innerHTML = ++timesClicked
+
+  setTimeout(() => heart.remove(), 1000)
 }
