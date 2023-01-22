5d94e5c55b5360cc873263aee11f8a73e9135fa7
Leo Wong <leoelicos@gmail.com>
2023-01-22 17:06:17 +1100

    Starter code

--- src/components/28__github-profiles/index.jsx
+++ src/components/28__github-profiles/index.jsx
@@ -1,3 +1,19 @@
+import './style/style.css'
 export default function GithubProfiles() {
-  return <div>GithubProfiles</div>
+  return (
+    <div className='app-28'>
+      <div className='body'>
+        <form
+          className='user-form'
+          id='form'>
+          <input
+            type='text'
+            id='search'
+            placeholder='Search a Github User'
+          />
+        </form>
+        <main id='main'></main>
+      </div>
+    </div>
+  )
 }
