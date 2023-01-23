46a7e2771932579331aab77aee9538a49ace2b11
Leo Wong <leoelicos@gmail.com>
2023-01-23 20:40:28 +1100

    Implement with useEffect and useState

--- src/components/33__notes-app/index.jsx
+++ src/components/33__notes-app/index.jsx
@@ -1,79 +1,97 @@
 import Markdown from 'marked-react'
+import { useEffect, useState } from 'react'
 
 import './style/style.css'
 
 export default function NotesApp() {
+  const [notes, setNotes] = useState([])
+  const handleTextChange = (text, timestamp) => {
+    setNotes((prev) => prev.map((v) => (v.timestamp === timestamp ? { ...v, text } : v)))
+  }
+  const handleDelete = (timestamp) => {
+    setNotes((prev) => prev.filter((v) => v.timestamp !== timestamp))
+  }
+  useEffect(() => {
+    setNotes(JSON.parse(localStorage.getItem('notes')))
+  }, [])
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes))
+  }, [notes])
+
+  useEffect(() => {
+    return () => {
+      const y = notes.filter((note) => {
+        const res = note.text.length > 0
+        console.log({ note, res })
+        return res
+      })
+      const x = JSON.stringify(y)
+      localStorage.setItem('notes', x)
+      debugger
+    }
+  }, [])
+
+  const addNote = () => {
+    setNotes(notes.slice().concat({ text: '', timestamp: new Date().getTime() }))
+  }
   return (
-    <div className='app-33'>
-      <div className='body'>
-        <button
-          className='add'
-          id='add'>
-          <i className='fas fa-plus'></i> Add note
-        </button>
-      </div>
-    </div>
+    <App>
+      <Body>
+        <AddButton addNote={addNote} />
+        {notes.map(({ text, timestamp }) => (
+          <Note
+            key={timestamp}
+            text={text}
+            timestamp={timestamp}
+            handleTextChange={handleTextChange}
+            handleDelete={handleDelete}
+          />
+        ))}
+      </Body>
+    </App>
   )
 }
 
-/* 
-
-const addBtn = document.getElementById('add');
-
-const notes = JSON.parse(localStorage.getItem('notes'));
-
-if (notes) {
-	notes.forEach((note) => addNewNote(note));
+const App = ({ children }) => <div className='app-33'>{children}</div>
+const Body = ({ children }) => <div className='body'>{children}</div>
+const AddButton = ({ addNote }) => (
+  <button
+    className='add'
+    onClick={addNote}>
+    <i className='fas fa-plus'></i> Add note
+  </button>
+)
+const Note = ({ text, timestamp, handleDelete, handleTextChange }) => {
+  const [editing, setEditing] = useState(text.length > 0 ? false : true)
+  return (
+    <div className='note'>
+      <div className='tools'>
+        <button
+          className='edit'
+          onClick={() => {
+            setEditing((prev) => !prev)
+          }}>
+          <i className='fas fa-edit'></i>
+        </button>
+        <button
+          className='delete'
+          onClick={() => {
+            handleDelete(timestamp)
+          }}>
+          <i className='fas fa-trash-alt'></i>
+        </button>
+      </div>
+      <div className={`main ${editing ? 'hidden' : ''}`}>
+        <Markdown>{text}</Markdown>
+      </div>
+      <textarea
+        className={editing ? '' : 'hidden'}
+        value={text}
+        onInput={(e) => {
+          handleTextChange(e.target.value, timestamp)
+        }}>
+        {text}
+      </textarea>
+    </div>
+  )
 }
-
-addBtn.addEventListener('click', () => addNewNote());
-
-function addNewNote(text = '') {
-	const note = document.createElement('div');
-	note.classList.add('note');
-	note.innerHTML = `
-   <div className="tools">
-      <button className="edit"><i className="fas fa-edit"></i></button>
-      <button className="delete"><i className="fas fa-trash-alt"></i></button>
-   </div>			
-   <div className="main ${text ? '' : 'hidden'}"></div>
-   <textarea className=" ${text ? 'hidden' : ''}"></textarea>
-   `;
-
-	const editBtn = note.querySelector('.edit');
-	const deleteBtn = note.querySelector('.delete');
-	const main = note.querySelector('.main');
-	const textArea = note.querySelector('textarea');
-
-	textArea.value = text;
-	main.innerHTML = marked.parse(text);
-
-	deleteBtn.addEventListener('click', () => {
-		note.remove();
-		updateLS();
-	});
-
-	editBtn.addEventListener('click', () => {
-		main.classList.toggle('hidden');
-		textArea.classList.toggle('hidden');
-	});
-
-	textArea.addEventListener('input', (e) => {
-		const { value } = e.target;
-		main.innerHTML = marked.parse(value);
-		updateLS();
-	});
-
-	document.body.appendChild(note);
-}
-
-function updateLS() {
-	const notesText = document.querySelectorAll('textarea');
-	const notes = [];
-	notesText.forEach((note) => notes.push(note.value));
-
-	localStorage.setItem('notes', JSON.stringify(notes));
-}
-
-
-*/
