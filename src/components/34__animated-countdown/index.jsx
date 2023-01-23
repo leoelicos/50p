323df6adbab04a494e83b7e5895b8b0148892d8f
Leo Wong <leoelicos@gmail.com>
2023-01-24 01:38:06 +1100

    Starter JSX and commented Starter JS

--- src/components/34__animated-countdown/index.jsx
+++ src/components/34__animated-countdown/index.jsx
@@ -1,3 +1,69 @@
+import './style/style.css'
 export default function AnimatedCountdown() {
-  return <div>AnimatedCountdown</div>
+  return (
+    <div className='app-34'>
+      <div className='body'>
+        <div className='counter'>
+          <div className='nums'>
+            <span className='in'>3</span>
+            <span>2</span>
+            <span>1</span>
+            <span>0</span>
+          </div>
+          <h4>Get Ready</h4>
+        </div>
+        <div className='final'>
+          <h1>GO</h1>
+          <button id='replay'>
+            <span>Replay</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
+
+/* 
+
+const nums = document.querySelectorAll('.nums span');
+const counter = document.querySelector('.counter');
+const finalMessage = document.querySelector('.final');
+const replay = document.querySelector('#replay');
+
+runAnimation();
+
+function resetDOM() {
+	counter.classList.remove('hide');
+	finalMessage.classList.remove('show');
+
+	nums.forEach((num) => {
+		num.classList.value = '';
+	});
+
+	nums[0].classList.add('in');
+}
+
+function runAnimation() {
+	nums.forEach((num, idx) => {
+		const nextToLast = nums.length - 1;
+		num.addEventListener('animationend', (e) => {
+			if (e.animationName === 'goIn' && idx !== nextToLast) {
+				num.classList.remove('in');
+				num.classList.add('out');
+			} else if (e.animationName === 'goOut' && num.nextElementSibling) {
+				num.nextElementSibling.classList.add('in');
+			} else {
+				counter.classList.add('hide');
+				finalMessage.classList.add('show');
+			}
+		});
+	});
+}
+
+replay.addEventListener('click', () => {
+	resetDOM();
+	runAnimation();
+});
+
+
+*/
