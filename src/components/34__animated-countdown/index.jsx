7fb3344bcdc7d012b75dbaf532449e795ceba8d2
Leo Wong <leoelicos@gmail.com>
2023-01-24 01:51:18 +1100

    Implement with useState

--- src/components/34__animated-countdown/index.jsx
+++ src/components/34__animated-countdown/index.jsx
@@ -1,9 +1,41 @@
+import { useState } from 'react'
 import './style/style.css'
 export default function AnimatedCountdown() {
+  const nums = document.querySelectorAll('.nums span')
+  const [final, setFinal] = useState(false)
+
+  runAnimation()
+
+  function resetDOM() {
+    setFinal(false)
+
+    nums.forEach((num) => {
+      num.classList.value = ''
+    })
+
+    nums[0].classList.add('in')
+  }
+
+  function runAnimation() {
+    nums.forEach((num, idx) => {
+      const nextToLast = nums.length - 1
+      num.addEventListener('animationend', (e) => {
+        if (e.animationName === 'goIn' && idx !== nextToLast) {
+          num.classList.remove('in')
+          num.classList.add('out')
+        } else if (e.animationName === 'goOut' && num.nextElementSibling) {
+          num.nextElementSibling.classList.add('in')
+        } else {
+          setFinal(true)
+        }
+      })
+    })
+  }
+
   return (
     <div className='app-34'>
       <div className='body'>
-        <div className='counter'>
+        <div className={`counter ${final ? 'hide' : ''}`}>
           <div className='nums'>
             <span className='in'>3</span>
             <span>2</span>
@@ -12,9 +44,14 @@ export default function AnimatedCountdown() {
           </div>
           <h4>Get Ready</h4>
         </div>
-        <div className='final'>
+        <div className={`final ${final ? 'show' : ''}`}>
           <h1>GO</h1>
-          <button id='replay'>
+          <button
+            id='replay'
+            onClick={() => {
+              resetDOM()
+              runAnimation()
+            }}>
             <span>Replay</span>
           </button>
         </div>
@@ -22,48 +59,3 @@ export default function AnimatedCountdown() {
     </div>
   )
 }
-
-/* 
-
-const nums = document.querySelectorAll('.nums span');
-const counter = document.querySelector('.counter');
-const finalMessage = document.querySelector('.final');
-const replay = document.querySelector('#replay');
-
-runAnimation();
-
-function resetDOM() {
-	counter.classList.remove('hide');
-	finalMessage.classList.remove('show');
-
-	nums.forEach((num) => {
-		num.classList.value = '';
-	});
-
-	nums[0].classList.add('in');
-}
-
-function runAnimation() {
-	nums.forEach((num, idx) => {
-		const nextToLast = nums.length - 1;
-		num.addEventListener('animationend', (e) => {
-			if (e.animationName === 'goIn' && idx !== nextToLast) {
-				num.classList.remove('in');
-				num.classList.add('out');
-			} else if (e.animationName === 'goOut' && num.nextElementSibling) {
-				num.nextElementSibling.classList.add('in');
-			} else {
-				counter.classList.add('hide');
-				finalMessage.classList.add('show');
-			}
-		});
-	});
-}
-
-replay.addEventListener('click', () => {
-	resetDOM();
-	runAnimation();
-});
-
-
-*/
