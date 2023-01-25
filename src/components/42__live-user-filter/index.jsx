d6e3128d9600a2bfd13812a513f689699389a4e3
Leo Wong <leoelicos@gmail.com>
2023-01-25 22:07:15 +1100

    Implement with useEffect, useState

--- src/components/42__live-user-filter/index.jsx
+++ src/components/42__live-user-filter/index.jsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from 'react'
+import './style/style.css'
 export default function LiveUserFilter() {
+  const [results, setResults] = useState([])
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch('https://randomuser.me/api?results=50')
+      const data = await res.json()
+      setResults(data.results)
+    }
+    getData()
+  }, [])
+
   return (
     <div className='app-42'>
       <div className='body'>
@@ -11,14 +25,43 @@ export default function LiveUserFilter() {
               name=''
               id='filter'
               placeholder='Search'
+              value={search}
+              onChange={(e) => {
+                setSearch(e.target.value)
+              }}
             />
           </header>
           <ul
             id='result'
             className='user-list'>
-            <li>
-              <h3>Loading…</h3>
-            </li>
+            {results ? (
+              results.map((user) => {
+                const userString = (user.name.first + user.name.last + user.location.city + user.location.country).toLowerCase()
+                const searchString = search.toLowerCase()
+                return (
+                  <li
+                    key={userString}
+                    className={userString.includes(searchString) ? '' : 'hide'}>
+                    <img
+                      src={user.picture.large}
+                      alt={user.name.first}
+                    />
+                    <div className='user-info'>
+                      <h4>
+                        {user.name.first} {user.name.last}
+                      </h4>
+                      <p>
+                        {user.location.city}, {user.location.country}
+                      </p>
+                    </div>
+                  </li>
+                )
+              })
+            ) : (
+              <li>
+                <h3>Loading…</h3>
+              </li>
+            )}
           </ul>
         </div>
       </div>
