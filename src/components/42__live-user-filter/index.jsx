5a68ad61b27f425ba5573c9867e83d24f672d39f
Leo Wong <leoelicos@gmail.com>
2023-01-25 21:03:43 +1100

    Starter JSX

--- src/components/42__live-user-filter/index.jsx
+++ src/components/42__live-user-filter/index.jsx
@@ -1,3 +1,27 @@
 export default function LiveUserFilter() {
-  return <div>LiveUserFilter</div>
+  return (
+    <div className='app-42'>
+      <div className='body'>
+        <div className='container'>
+          <header className='header'>
+            <h4 className='title'>Live User Filter</h4>
+            <small className='subtitle'>Search by name and/or location</small>
+            <input
+              type='text'
+              name=''
+              id='filter'
+              placeholder='Search'
+            />
+          </header>
+          <ul
+            id='result'
+            className='user-list'>
+            <li>
+              <h3>Loading…</h3>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
 }
