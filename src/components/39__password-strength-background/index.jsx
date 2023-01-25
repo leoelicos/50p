9b137cf12e7886ea0c3d26d86e73c958c0a87fb7
Leo Wong <leoelicos@gmail.com>
2023-01-25 15:31:56 +1100

    Implement blur effect with useState

--- src/components/39__password-strength-background/index.jsx
+++ src/components/39__password-strength-background/index.jsx
@@ -1,17 +1,22 @@
+import { useState } from 'react'
 import './style/style.css'
 export default function PasswordStrengthBackground() {
+  const [blur, setBlur] = useState(0)
+  const [input, setInput] = useState('')
+
   return (
     <div className='app-39'>
       <div className='body'>
         <div
           className='background'
-          id='background'></div>
+          id='background'
+          style={{ filter: `blur(${blur}px)` }}></div>
         <div className='bg-white rounded p-10 text-center shadow-md'>
           <h1 className='text-3xl'>Image Password Strength</h1>
           <p className='text-sm text-gray-700'>Change the password to see the effect</p>
           <div className='my-4 text-left'>
             <label
-              for='email'
+              htmlFor='email'
               className='text-gray-900'>
               Email
             </label>
@@ -25,7 +30,7 @@ export default function PasswordStrengthBackground() {
 
           <div className='my-4 text-left'>
             <label
-              for='email'
+              htmlFor='email'
               className='text-gray-900'>
               Password:
             </label>
@@ -34,6 +39,15 @@ export default function PasswordStrengthBackground() {
               className='border block w-full p-2 mt-2 rounded'
               id='password'
               placeholder='Enter Password'
+              value={input}
+              onInput={(e) => {
+                const input = e.target.value
+                console.log(input)
+                setInput(input)
+                const length = input.length
+                const blurValue = 20 - length * 2
+                setBlur(blurValue)
+              }}
             />
           </div>
 
