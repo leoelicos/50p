1061bbe9f1d8534eb38cb95bcf33b8934222f2c9
Leo Wong <leoelicos@gmail.com>
2023-01-25 20:49:38 +1100

    gender neutrality

--- src/components/41__verify-account-ui/index.jsx
+++ src/components/41__verify-account-ui/index.jsx
@@ -35,7 +35,7 @@ export default function VerifyAccountUI() {
         <div className='container'>
           <h2>Verify Your Account</h2>
           <p>
-            We emailed you the six digit code to cool_guy@email.com <br />
+            We emailed you the six digit code to cool_person@email.com <br />
             Enter the code below to confirm your email address.
           </p>
           <div className='code-container'>
