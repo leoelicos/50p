135e05eee8db0f08cbe60ec7a1cf15b28636943b
Leo Wong <leoelicos@gmail.com>
2023-01-25 15:34:54 +1100

    Starter JSX

--- src/components/40__3d-background-boxes/index.jsx
+++ src/components/40__3d-background-boxes/index.jsx
@@ -1,3 +1,17 @@
+import './style/style.css'
 export default function _3DBackgroundBoxes() {
-  return <div>_3DBackgroundBoxes</div>
+  return (
+    <div className='app-40'>
+      <div className='body'>
+        <button
+          id='btn'
+          className='magic'>
+          Magic ⚫
+        </button>
+        <div
+          id='boxes'
+          className='boxes big'></div>
+      </div>
+    </div>
+  )
 }
