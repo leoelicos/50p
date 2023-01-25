f48e1552927135242b1f950a1b3665cc37b7a251
Leo Wong <leoelicos@gmail.com>
2023-01-25 12:04:03 +1100

    Move api to separate file

--- src/components/37__pokedex/index.jsx
+++ src/components/37__pokedex/index.jsx
@@ -1,53 +1,12 @@
 import { useEffect, useRef, useState } from 'react'
 import './style/style.css'
+import pokemonPromises from './api'
+
 export default function Pokedex() {
-  const componentDidMount = useRef(false)
-  const colors = {
-    fire: '#FDDFDF',
-    grass: '#DEFDE0',
-    electric: '#FCF7DE',
-    water: '#DEF3FD',
-    ground: '#F4E7DA',
-    rock: '#D5D5D4',
-    fairy: '#FCEAFF',
-    poison: '#98D7A5',
-    bug: '#F8D5A3',
-    dragon: '#97B3E6',
-    psychic: '#EAEDA1',
-    flying: '#F5F5F5',
-    fighting: '#E6E0D4',
-    normal: '#F5F5F5'
-  }
-
-  const main_types = Object.keys(colors)
-
   const [pokemons, setPokemons] = useState(null)
+  const componentDidMount = useRef(false)
 
   useEffect(() => {
-    const pokemonPromises = []
-    for (let i = 1; i <= 150; i++) {
-      pokemonPromises.push(
-        new Promise(async (resolve, reject) => {
-          const url = `https://pokeapi.co/api/v2/pokemon/${i}`
-          try {
-            const r1 = await fetch(url)
-            const pokemon = await r1.json()
-
-            const img = pokemon.sprites.front_default
-            const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
-            const id = pokemon.id.toString().padStart(3, '0')
-
-            const poke_types = pokemon.types.map((type) => type.type.name)
-            const type = main_types.find((type) => poke_types.indexOf(type) > -1)
-            const color = colors[type]
-            const response = { id, img, name, type, color }
-            resolve(response)
-          } catch (err) {
-            reject(err)
-          }
-        })
-      )
-    }
     const fetchPokemons = async () => {
       const data = await Promise.all(pokemonPromises)
       setPokemons(data)
