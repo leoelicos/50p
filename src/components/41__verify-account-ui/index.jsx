38f0af0ac7978b27575ca8af2de7dab5dd0a67f0
Leo Wong <leoelicos@gmail.com>
2023-01-25 21:01:24 +1100

    Abstracting state: digits and maps

--- src/components/41__verify-account-ui/index.jsx
+++ src/components/41__verify-account-ui/index.jsx
@@ -3,13 +3,7 @@ import './style/style.css'
 
 export default function VerifyAccountUI() {
   const inputRefs = useRef([])
-
-  const [v0, setV0] = useState('')
-  const [v1, setV1] = useState('')
-  const [v2, setV2] = useState('')
-  const [v3, setV3] = useState('')
-  const [v4, setV4] = useState('')
-  const [v5, setV5] = useState('')
+  const [digits, setDigits] = useState(['', '', '', '', '', ''])
 
   const getNextInput = (currentIdx) => inputRefs.current[currentIdx < 5 ? currentIdx + 1 : 5]
 
@@ -27,107 +21,25 @@ export default function VerifyAccountUI() {
             Enter the code below to confirm your email address.
           </p>
           <div className='code-container'>
-            <input
-              ref={(e) => inputRefs.current.push(e)}
-              type='number'
-              className='code'
-              placeholder='0'
-              min='0'
-              max='9'
-              required
-              value={v0}
-              onChange={(e) => {
-                if (e.nativeEvent.data >= 0 && e.nativeEvent.data <= 9) {
-                  setV0(e.nativeEvent.data)
-
-                  getNextInput(0).focus()
-                }
-              }}
-            />
-            <input
-              ref={(e) => inputRefs.current.push(e)}
-              type='number'
-              className='code'
-              placeholder='0'
-              min='0'
-              max='9'
-              required
-              value={v1}
-              onChange={(e) => {
-                if (e.nativeEvent.data >= 0 && e.nativeEvent.data <= 9) {
-                  setV1(e.nativeEvent.data)
-
-                  getNextInput(1).focus()
-                }
-              }}
-            />
-            <input
-              ref={(e) => inputRefs.current.push(e)}
-              type='number'
-              className='code'
-              placeholder='0'
-              min='0'
-              max='9'
-              required
-              value={v2}
-              onChange={(e) => {
-                if (e.nativeEvent.data >= 0 && e.nativeEvent.data <= 9) {
-                  setV2(e.nativeEvent.data)
-
-                  getNextInput(2).focus()
-                }
-              }}
-            />
-            <input
-              ref={(e) => inputRefs.current.push(e)}
-              type='number'
-              className='code'
-              placeholder='0'
-              min='0'
-              max='9'
-              required
-              value={v3}
-              onChange={(e) => {
-                if (e.nativeEvent.data >= 0 && e.nativeEvent.data <= 9) {
-                  setV3(e.nativeEvent.data)
-
-                  getNextInput(3).focus()
-                }
-              }}
-            />
-            <input
-              ref={(e) => inputRefs.current.push(e)}
-              type='number'
-              className='code'
-              placeholder='0'
-              min='0'
-              max='9'
-              required
-              value={v4}
-              onChange={(e) => {
-                if (e.nativeEvent.data >= 0 && e.nativeEvent.data <= 9) {
-                  setV4(e.nativeEvent.data)
-
-                  getNextInput(4).focus()
-                }
-              }}
-            />
-            <input
-              ref={(e) => inputRefs.current.push(e)}
-              type='number'
-              className='code'
-              placeholder='0'
-              min='0'
-              max='9'
-              required
-              value={v5}
-              onChange={(e) => {
-                if (e.nativeEvent.data >= 0 && e.nativeEvent.data <= 9) {
-                  setV5(e.nativeEvent.data)
-                  getNextInput(4).focus()
-                }
-              }}
-            />
+            {digits.map((v, i) => (
+              <input
+                key={i}
+                ref={(e) => inputRefs.current.push(e)}
+                type='number'
+                className='code'
+                placeholder='0'
+                min='0'
+                max='9'
+                required
+                value={v}
+                onChange={(e) => {
+                  if (e.nativeEvent.data >= 0 && e.nativeEvent.data <= 9) {
+                    setDigits((prev) => prev.map((val, idx) => (idx === i ? e.nativeEvent.data : val)))
+                    getNextInput(i).focus()
+                  }
+                }}
+              />
+            ))}
           </div>
           <small className='info'> This is design only. We didn't actually send you an email as we don't have your email, right? </small>
         </div>
