c827ae4bd0bc6dd1f91ca3ab147a571833309ec5
Leo Wong <leoelicos@gmail.com>
2023-01-26 15:38:28 +1100

    Implement with useState

--- src/components/43__feedback-ui-design/index.jsx
+++ src/components/43__feedback-ui-design/index.jsx
@@ -1,42 +1,63 @@
+import { useState } from 'react'
+import './style/style.css'
+const data = [
+  { src: 'https://img.icons8.com/emoji/256/disappointed-face.png', alt: 'unhappy' },
+  { src: 'https://storage.googleapis.com/emojibuff.appspot.com/i/go/1f610.png', alt: 'neutral' },
+  { src: 'https://www.virtualverse.one/forums/styles/themehouse/reactions/happy.png', alt: 'satisfied' }
+]
+
 export default function FeedbackUIDesign() {
+  const [sent, setSent] = useState(false)
+
+  const [active, setActive] = useState(null)
+  const selectedRating = data.find((v, i) => active === i)?.alt ?? ''
   return (
     <div className='app-43'>
       <div className='body'>
         <div
           id='panel'
           className='panel-container'>
-          <strong>
-            How satisfied are you with our <br />
-            customer support performance?
-          </strong>
-          <div className='ratings-container'>
-            <div className='rating'>
-              <img
-                src='https://img.icons8.com/emoji/256/disappointed-face.png'
-                alt='unhappy'
-              />
-              <small>Unhappy</small>
-            </div>
-            <div className='rating'>
-              <img
-                src='https://storage.googleapis.com/emojibuff.appspot.com/i/go/1f610.png'
-                alt='neutral'
-              />
-              <small>Neutral</small>
-            </div>
-            <div className='rating'>
-              <img
-                src='https://www.virtualverse.one/forums/styles/themehouse/reactions/happy.png'
-                alt='satisfied'
-              />
-              <small>Satisfied</small>
-            </div>
-          </div>
-          <button
-            className='btn'
-            id='send'>
-            Send Review
-          </button>
+          {sent ? (
+            <>
+              <i className='fas fa-heart' />
+              <strong>Thank you!</strong>
+              <br />
+              <strong>Feedback: {selectedRating}</strong>
+              <p>We'll use your feedback to improve our customer support</p>
+            </>
+          ) : (
+            <>
+              <strong>
+                How satisfied are you with our <br />
+                customer support performance?
+              </strong>
+              <div className='ratings-container'>
+                {data.map(({ src, alt }, i) => (
+                  <button
+                    key={alt}
+                    className={`rating ${active === i ? 'active' : ''}`}
+                    onClick={() => {
+                      setActive(i)
+                    }}>
+                    <img
+                      src={src}
+                      alt={src}
+                    />
+                    <small>{alt[0].toUpperCase() + alt.slice(1)}</small>
+                  </button>
+                ))}
+              </div>
+
+              <button
+                className='btn'
+                id='send'
+                onClick={() => {
+                  setSent(true)
+                }}>
+                Send Review
+              </button>
+            </>
+          )}
         </div>
       </div>
     </div>
