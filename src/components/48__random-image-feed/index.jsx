a3a3bfd2c78a2a36055a2709267968dd3a7c9e77
Leo Wong <leoelicos@gmail.com>
2023-01-26 20:10:40 +1100

    Starter JSX

--- src/components/48__random-image-feed/index.jsx
+++ src/components/48__random-image-feed/index.jsx
@@ -1,3 +1,10 @@
 export default function RandomImageFeed() {
-  return <div>RandomImageFeed</div>
+  return (
+    <div className='app-48'>
+      <div className='body'>
+        <h1 className='title'>Random Image Feed</h1>
+        <div className='container'></div>
+      </div>
+    </div>
+  )
 }
