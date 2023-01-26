cb9125a969da4df77c9c76d253204016ac3a5b31
Leo Wong <leoelicos@gmail.com>
2023-01-26 21:14:33 +1100

    Starter JSX

--- src/components/50__insect-catch-game/index.jsx
+++ src/components/50__insect-catch-game/index.jsx
@@ -1,3 +1,26 @@
+import './style/style.css'
 export default function InsectCatchGame() {
-  return <div>InsectCatchGame</div>
+  return (
+    <div className='app-50'>
+      <div className='body'>
+        <h1>todos</h1>
+        <form id='form'>
+          <input
+            type='text'
+            id='input'
+            className='input'
+            placeholder='Enter your todo'
+            autocomplete='off'
+          />
+          <ul
+            className='todos'
+            id='todos'></ul>
+        </form>
+        <small>
+          left click to toggle completed. <br />
+          Right click to delete todo
+        </small>
+      </div>
+    </div>
+  )
 }
