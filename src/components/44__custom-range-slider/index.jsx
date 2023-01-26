68b1926b88bbc7558c9a6df72a8e639077520860
Leo Wong <leoelicos@gmail.com>
2023-01-26 15:41:57 +1100

    Starter JSX

--- src/components/44__custom-range-slider/index.jsx
+++ src/components/44__custom-range-slider/index.jsx
@@ -1,3 +1,19 @@
+import './style/style.css'
 export default function CustomRangeSlider() {
-  return <div>CustomRangeSlider</div>
+  return (
+    <div className='app-44'>
+      <div className='body'>
+        <h2>Custom Range Slider</h2>
+        <div className='range-container'>
+          <input
+            type='range'
+            id='range'
+            min='0'
+            max='100'
+          />
+          <label for='range'>50</label>
+        </div>
+      </div>
+    </div>
+  )
 }
