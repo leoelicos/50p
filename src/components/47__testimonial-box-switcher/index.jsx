fd9cfde357edee11e264cef3743374d85287eebd
Leo Wong <leoelicos@gmail.com>
2023-01-26 19:13:25 +1100

    Starter JSX

--- src/components/47__testimonial-box-switcher/index.jsx
+++ src/components/47__testimonial-box-switcher/index.jsx
@@ -1,3 +1,25 @@
 export default function TestimonialBoxSwitcher() {
-  return <div>TestimonialBoxSwitcher</div>
+  return (
+    <div className='app-47'>
+      <div className='body'>
+        <div className='testimonial-container'>
+          <div className='progress-bar'></div>
+          <div className='fas fa-quote-left fa-quote'></div>
+          <div className='fas fa-quote-right fa-quote'></div>
+          <p className='testimonial'></p>
+          <div className='user'>
+            <img
+              src=''
+              alt=''
+              className='user-image'
+            />
+            <div className='user-details'>
+              <h4 className='username'></h4>
+              <p className='role'></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
