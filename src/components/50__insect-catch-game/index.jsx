ad764b7aab437ad32bc3ca2ee372dd1e3c35d0ff
Leo Wong <leoelicos@gmail.com>
2023-01-26 21:15:18 +1100

    Fixed starter JS

--- src/components/50__insect-catch-game/index.jsx
+++ src/components/50__insect-catch-game/index.jsx
@@ -3,23 +3,76 @@ export default function InsectCatchGame() {
   return (
     <div className='app-50'>
       <div className='body'>
-        <h1>todos</h1>
-        <form id='form'>
-          <input
-            type='text'
-            id='input'
-            className='input'
-            placeholder='Enter your todo'
-            autocomplete='off'
-          />
-          <ul
-            className='todos'
-            id='todos'></ul>
-        </form>
-        <small>
-          left click to toggle completed. <br />
-          Right click to delete todo
-        </small>
+        <div class='screen'>
+          <h1>Catch The Insect</h1>
+          <button
+            class='btn'
+            id='start-btn'>
+            Play Game
+          </button>
+        </div>
+
+        <div class='screen'>
+          <h1>What is your "favourite" insect?</h1>
+          <ul class='insects-list'>
+            <li>
+              <button class='choose-insect-btn'>
+                <p>Fly</p>
+                <img
+                  src='http://pngimg.com/uploads/fly/fly_PNG3946.png'
+                  alt='fly'
+                />
+              </button>
+            </li>
+            <li>
+              <button class='choose-insect-btn'>
+                <p>Mosquito</p>
+                <img
+                  src='http://pngimg.com/uploads/mosquito/mosquito_PNG18175.png'
+                  alt='mosquito'
+                />
+              </button>
+            </li>
+            <li>
+              <button class='choose-insect-btn'>
+                <p>Spider</p>
+                <img
+                  src='http://pngimg.com/uploads/spider/spider_PNG12.png'
+                  alt='spider'
+                />
+              </button>
+            </li>
+            <li>
+              <button class='choose-insect-btn'>
+                <p>Roach</p>
+                <img
+                  src='http://pngimg.com/uploads/roach/roach_PNG12163.png'
+                  alt='roch'
+                />
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div
+          id='game-container'
+          class='screen game-container'>
+          <h3
+            id='time'
+            class='time'>
+            Time: 00:00
+          </h3>
+          <h3
+            id='score'
+            class='score'>
+            Score: 0
+          </h3>
+          <h5
+            id='message'
+            class='message'>
+            Are you annoyed yet? <br />
+            You are playing an impossible game!!
+          </h5>
+        </div>
       </div>
     </div>
   )
