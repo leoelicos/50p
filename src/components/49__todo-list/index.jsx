71104e78bbad65d8d81507fc6e9021d040884549
Leo Wong <leoelicos@gmail.com>
2023-01-26 20:30:00 +1100

    Starter JSX

--- src/components/49__todo-list/index.jsx
+++ src/components/49__todo-list/index.jsx
@@ -1,3 +1,25 @@
 export default function TodoList() {
-  return <div>TodoList</div>
+  return (
+    <div className='app-49'>
+      <div className='body'>
+        <h1>todos</h1>
+        <form id='form'>
+          <input
+            type='text'
+            id='input'
+            className='input'
+            placeholder='Enter your todo'
+            autocomplete='off'
+          />
+          <ul
+            className='todos'
+            id='todos'></ul>
+        </form>
+        <small>
+          left click to toggle completed. <br />
+          Right click to delete todo
+        </small>
+      </div>
+    </div>
+  )
 }
