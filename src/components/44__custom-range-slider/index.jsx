83a8cd819b6eb56cfcdcd596323564a58ebb6c2c
Leo Wong <leoelicos@gmail.com>
2023-01-26 15:53:13 +1100

    Implement with useRef, useState

--- src/components/44__custom-range-slider/index.jsx
+++ src/components/44__custom-range-slider/index.jsx
@@ -1,5 +1,13 @@
+import { useRef, useState } from 'react'
 import './style/style.css'
 export default function CustomRangeSlider() {
+  const [range, setRange] = useState(50)
+  const [offset, setOffset] = useState(null)
+  const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+  }
+  const labelRef = useRef(null)
+
   return (
     <div className='app-44'>
       <div className='body'>
@@ -10,8 +18,26 @@ export default function CustomRangeSlider() {
             id='range'
             min='0'
             max='100'
+            value={range}
+            onChange={(e) => {
+              setRange(e.target.value)
+              const value = +e.target.value
+              const range_width = getComputedStyle(e.target).getPropertyValue('width')
+              const label_width = getComputedStyle(labelRef.current).getPropertyValue('width')
+              const num_width = +range_width.substring(0, range_width.length - 2)
+              const num_label_width = +label_width.substring(0, label_width.length - 2)
+              const max = e.target.max // 100
+              const min = e.target.min // 0
+              const left = value * (num_width / max) - num_label_width / 2 + scale(value, min, max, 10, -10)
+              setOffset(left)
+            }}
           />
-          <label for='range'>50</label>
+          <label
+            ref={labelRef}
+            for='range'
+            style={{ left: `${offset}px` }}>
+            {range}
+          </label>
         </div>
       </div>
     </div>
