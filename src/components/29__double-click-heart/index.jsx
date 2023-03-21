fab441295e84ad0b7fe271367bc117751f1b98ff
Leo Wong <leoelicos@gmail.com>
2023-03-21 23:56:17 +1100

    Fixed heart not rendering properly

--- src/components/29__double-click-heart/index.jsx
+++ src/components/29__double-click-heart/index.jsx
@@ -1,39 +1,55 @@
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react'
 import './style/style.css'
 
 export default function DoubleClickHeart() {
-  const [clickTime, setClickTime] = useState(0)
+  const [lastClickTime, setLastClickTime] = useState(0)
   const [timesClicked, setTimesClicked] = useState(0)
 
   const [hearts, setHearts] = useState([])
 
-  const removeHeart = (timestamp) => setHearts((prev) => prev.filter((heart) => heart.timestamp !== timestamp))
-  const addHeart = (yInside, xInside, timestamp) => setHearts((prev) => prev.slice().concat({ yInside, xInside, timestamp }))
+  const removeHeart = (timestamp) => {
+    setHearts((prev) => {
+      const next = prev.filter((heart) => heart.timestamp !== timestamp)
+      console.log({ function: 'setHearts', next })
+      return next
+    })
+  }
+  const addHeart = (yInside, xInside, timestamp) => {
+    setHearts((prev) => {
+      const next = prev.slice().concat({ yInside, xInside, timestamp })
+
+      return next
+    })
+  }
 
   const handleClick = (e) => {
-    let t = new Date().getTime()
-    if (clickTime > 0 && t - clickTime < 800) {
-      const heart = document.createElement('i')
-      heart.classList.add('fas')
-      heart.classList.add('fa-heart')
-
+    let thisClickTime = new Date().getTime()
+    console.log({ lastClickTime, thisClickTime })
+    if (thisClickTime - lastClickTime < 800) {
       addHeart(e.clientY - e.target.offsetTop, e.clientX - e.target.offsetLeft, new Date().getTime())
 
       setTimesClicked((prev) => prev + 1)
 
-      setTimeout(removeHeart, 1000)
-
-      setClickTime(0)
-    } else {
-      setClickTime(t)
+      setTimeout(() => {
+        removeHeart(thisClickTime)
+      }, 3000)
     }
+
+    setLastClickTime(thisClickTime)
   }
 
   return (
     <App>
       <Body>
         <h3>
-          Double click on the image to <i className='fas fa-heart'></i> it
+          Double click on the image to{' '}
+          <FontAwesomeIcon
+            icon={faHeart}
+            className='red'
+          />{' '}
+          it
         </h3>
         <small>
           You liked it <Times timesClicked={timesClicked} /> times
@@ -57,9 +73,10 @@ const LoveMe = ({ handleClick, hearts }) => {
       className='loveMe'
       onClick={handleClick}>
       {hearts.map(({ timestamp, xInside, yInside }) => (
-        <i
-          className='fas fa-heart'
-          style={{ top: `${yInside}px`, left: `${xInside}px` }}
+        <FontAwesomeIcon
+          icon={faHeart}
+          className='heart red'
+          style={{ top: `${yInside}px`, left: `${xInside}px`, zIndex: 1 }}
           key={timestamp}
         />
       ))}
